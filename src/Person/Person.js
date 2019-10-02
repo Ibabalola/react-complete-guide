30539a1f59af8ae5bf8cfcd485a4dc8d4f2ada3f
Ibabalola <info@isaacbabalola.co.uk>
2019-10-02 19:44:15 +0100

    ejecting react app

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Person.css';
+import styles from './Person.module.css';
 
 /**
  * Functional components
@@ -13,7 +13,7 @@ import './Person.css';
  */
 const person = props => {
     return (
-        <div className="Person">
+        <div className={styles.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
