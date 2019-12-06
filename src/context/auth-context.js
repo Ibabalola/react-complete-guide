be3bc771b8155a993ce2eaf4fa25e93928a64140
Isaac Babalola <info@isaacbabalola.co.uk>
2019-12-06 17:03:36 +0000

    Completing component deep dive hooks

---
+++ src/context/auth-context.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// React createContext allows us to initialise our context with a default value
+// The context is globally available object
+// Its availablity can defined
+// It can also have an array, string a number as a value
+
+const authContext = React.createContext({
+    authenticated: false,
+    login: () => {}
+});
+
+export default authContext;
